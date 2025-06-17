943333bdddb6e789067fc95f8c4eec2ede061150
Jaskirat <singh3jt@uc.mail.edu>
2025-06-16 22:23:06 -0400

    Init Commit

--- src/Main.java
+++ src/Main.java
@@ -40,5 +40,19 @@ public class Main {
         public static void main(String[] args) {
 
         }
+        int numberOfCars = 3;
+
+        boolean isHailing = false;
+
+        double packPokemonCost = 7.50;
+
+        String favoriteColor = "red";
+
+        double burgerMcdonaldPrice = 4.99;
+
+        String fathersBirthMonth = "June";
+
+        boolean amHungry = false;
+
     }
 

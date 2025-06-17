f0af4e1ebe6de592affa05709531097a279bb872
Jaskirat <singh3jt@uc.mail.edu>
2025-06-16 23:12:07 -0400

    Lab_04_01_SalesTax

---
+++ src/Lab_04_01_SalesTax.java
@@ -0,0 +1,16 @@
+
+
+public class Lab_04_01_SalesTax {
+    public static void main(String[] args) {
+
+
+        double price = 200;
+
+        double tax;
+        tax = 0.05 * price;
+
+        System.out.println("Price: $" + price);
+        System.out.println("Sales tax (5%): $" + tax);
+    }
+}
+

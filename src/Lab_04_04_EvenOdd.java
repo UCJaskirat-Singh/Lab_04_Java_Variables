89ed2fb1ccd82540b843347850b2bc36937a2938
Jaskirat <singh3jt@uc.mail.edu>
2025-06-16 23:33:53 -0400

    Lab_04_04_EvenOdd

---
+++ src/Lab_04_04_EvenOdd.java
@@ -0,0 +1,12 @@
+public class Lab_04_04_EvenOdd {
+    public static void main(String[] args) {
+        int numExample = 3;
+
+        if (numExample % 2 == 0) {
+            System.out.println(numExample + " is even.");
+        } else {
+            System.out.println(numExample + " is odd.");
+        }
+    }
+}
+
